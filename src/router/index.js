3580be777bc22491205e28e214993c4b1e8fcf89
geromequa <gerome.quantmeyer@hotmail.com>
2025-01-22 16:05:12 +0100

    Added NotFoundView

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,7 @@
 import { createRouter, createWebHistory } from "vue-router";
 import SearchView from "@/views/SearchView.vue";
 import MovieView from "@/views/MovieView.vue";
+import NotFoundView from "@/views/NotFoundView.vue";
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -15,6 +16,11 @@ const router = createRouter({
       name: "movie-details",
       component: MovieView,
       props: true,
+    },
+    {
+      path: "/:catchAll(.*)",
+      name: "not-found",
+      component: NotFoundView,
     },
   ],
 });
---
+++ src/views/NotFoundView.vue
@@ -0,0 +1,12 @@
+<script setup>
+import { RouterLink } from "vue-router";
+</script>
+
+<template>
+  <div class="flex flex-col justify-center items-center h-96 text-white">
+    <span class="pi pi-exclamation-triangle text-primary text-7xl"></span>
+    <h1 class="text-5xl font-bold mb-4">404 Not Found</h1>
+    <p class="text-xl mb-5">This page does not exist</p>
+    <RouterLink to="/"> Go Back</RouterLink>
+  </div>
+</template>
